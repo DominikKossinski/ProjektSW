09830d17da5956c7d8c0f1259e3cb954c7101bc5
SzymG <szymon.gagorek@gmail.com>
2018-12-29 23:49:05 +0100

    Properly working charts reading from database

--- src/main/resources/static/js/measurements.js
+++ src/main/resources/static/js/measurements.js
@@ -1,6 +1,6 @@
-setTimeout(function () {
+/*setTimeout(function () {
     location.reload();
-}, 30000);
+}, 30000);*/
 
 function startCheckingLogin() {
     setTimeout("waitForLogout()", 2000);
@@ -33,43 +33,66 @@ function logout() {
 }
 
 function start() {
-    
+
     setTimeout(waitForLogout(), 2000);
-    getAllMeasurements();
-	getLastMeasurements();
+    getLastMeasurements();
 }
 
 function draw(measurements) {
-	
-    var CHART = document.getElementById("myCanvas");
-	console.log(CHART);
-	var lineChart = new Chart(CHART,{
-		type: 'line',
-		data: {
-			labels: ["A","B","C","D","E","F","G"],
-			datasets: [ 
-				{
-					label: "Temperatura w pomieszczeniu",
-					fill: false,
-					lineTension: 0.1,
-					borderColor: "#00ff55",
-					borderCapStyle: 'butt',
-					borderDash: [],
-					borderDashOffset: 0.0,
-					borderJoinStyle: 'miter',
-					pointBorderColor: "#ffffff",
-					pointBackgroundColor: "fff",
-					pointBorderWidth: 1,
-					pointHoverRadius: 5,
-					pointHoverBackgroundColor: "#3399ff",
-					pointHoverBorderColor: "#000000",
-					pointHoverBorderWidth: 2,
-					pointRadius: 6,
-					pointHitRadius: 10,
-					data: [65, 59, 80, 81, 56, 55, 40],
-				}
-			]
-		},
+
+    var temp = [];
+    var humi = [];
+    var x_temp = [];
+    var x_humi = [];
+
+    measurements.map(function (measurement) {
+
+        var d = new Date();
+        d = measurement.date;
+        x_temp.push(measurement.date);
+        x_humi = x_temp;
+        temp.push(measurement.temperature);
+        humi.push(measurement.humidity);
+        var h = measurement.temperature;
+
+    });
+
+    drawCharts(temp, x_temp, "myCanvas", "Temperatura w pomieszczeniu");
+    drawCharts(humi, x_humi, "myHumCanvas", "Wilgotność w pomieszczeniu");
+
+}
+
+function drawCharts(dat, x_tick, element, label_title) {
+
+    var CHART = document.getElementById(element);
+    console.log(CHART);
+    var lineChart = new Chart(CHART,{
+        type: 'line',
+        data: {
+            labels: x_tick,
+            datasets: [
+                {
+                    label: label_title,
+                    fill: false,
+                    lineTension: 0.1,
+                    borderColor: "#00ff55",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "#ffffff",
+                    pointBackgroundColor: "fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "#3399ff",
+                    pointHoverBorderColor: "#000000",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 6,
+                    pointHitRadius: 10,
+                    data: dat,
+                }
+            ]
+        },
         options: {
             legend: {
                 labels: {
@@ -81,24 +104,18 @@ function draw(measurements) {
                 yAxes: [{
                     ticks: {
                         fontColor: "white",
+                        padding: 30
                     }
                 }],
                 xAxes: [{
                     ticks: {
-                        fontColor: "white",
+                        fontColor: "white"
                     }
                 }]
             }
         }
-	});
-
-	
-	x.map(function (measurement) {
-			var h = measurement.temperature;
-	});
-	
+    });
 }
-
 
 function getAllMeasurements() {
 
@@ -128,7 +145,7 @@ function getMeasuremeantsByDate(date) {
     }).then(function (data) {
         console.log(data);
         if (data.responseStatus === "ok") {
-			
+
             return data.measurements;
         }
 
@@ -141,7 +158,7 @@ function getLastMeasurements() {
     }).then(function (data) {
         console.log(data);
         if (data.responseStatus === "ok") {
-			draw(data.measurements);
+            draw(data.measurements);
             return data.measurements;
         }
 
